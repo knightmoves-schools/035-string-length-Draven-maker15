765f0f73c6539fb251f807420f1b07b45ecdae02
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-04-26 08:17:20 -0500

    Add quote length calculation and output display
    Calculate the length of the quote and display it along with the 22nd letter.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
-
-//Add code below
+let quoteLength = quote.length;
+let output = quoteLength
+output = `${quoteLength} [${twentySecondLetter}]`;
+document.getElementById("result").innerHTML = output;
