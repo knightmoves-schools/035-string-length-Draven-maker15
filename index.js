e2986eacbb5dcb5b537b34dc627c6a0e97e10ce9
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-04-26 08:21:09 -0500

    Fix quote length output calculation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
+
+
 let quoteLength = quote.length;
 let output = quoteLength
 output = `${quoteLength} [${twentySecondLetter}]`;
